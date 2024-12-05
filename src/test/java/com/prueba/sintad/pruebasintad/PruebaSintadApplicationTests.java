34b765989282987f85c76bf4d048f57d43b20095
Jefferson Panta <pantajefferson173@gmail.com>
2024-12-05 18:41:45 -0500

    estructura de proyecto iniciada

---
+++ src/test/java/com/prueba/sintad/pruebasintad/PruebaSintadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.sintad.pruebasintad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSintadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
